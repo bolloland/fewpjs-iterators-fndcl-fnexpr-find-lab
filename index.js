be22177750c828fc8a0168227531752dc950a490
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-23 22:29:20 -0500

    commit tree e3be3c9fa064a96af1f5bf7a43021db85f4922c5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+myArray = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "X"},
+  {year: "2016", result: "N/A"}
+]
 
+
+function superbowlWin(array) {
+  let yearWon = array.find(obj => obj["result"] === "W")
+  if (yearWon) {
+      console.log(yearWon["year"])
+      return yearWon["year"]
+  } 
+}
+
+  // for(const obj of array) {
+    //     obj.find( (res) => {
+    //         obj["result"] === "W"
+    //     })
+    // }
